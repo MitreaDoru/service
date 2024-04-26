077f4c70a3ffb65b125ed4387ee8b57749b0d2a3
MitreaDoru <mitrea.doru.catalin@gmail.com>
2024-04-26 14:52:47 +0300

    Initial

--- app.js
+++ app.js
@@ -84,5 +84,5 @@ app.use(appointmentsRoutes)
 
 mongoose.connect(MONGODB_URI)
     .then(result => {
-        app.listen(8000)
+        httpServer.listen(8000)
     }).catch(err => console.log(err))
