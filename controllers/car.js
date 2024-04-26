3323f4c4f2b1971e88ae8cb962e939f055653d3d
MitreaDoru <mitrea.doru.catalin@gmail.com>
2024-04-26 17:30:18 +0300

    Initial

--- controllers/car.js
+++ controllers/car.js
@@ -12,7 +12,6 @@ exports.postAddCar = (req, res, next) => {
     const capacitateMotor = req.body.capacitateMotor;
     const caiPutere = req.body.caiPutere;
     const kWPutere = req.body.kWPutere;
-    const istoricService = req.body.istoricService
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.json({
@@ -38,8 +37,8 @@ exports.postAddCar = (req, res, next) => {
         anulFabricatiei: anulFabricatiei,
         tipMotorizare: tipMotorizare,
         capacitateMotor: capacitateMotor,
-        caiPutere: caiPutere ? caiPutere : kWPutere * 1.36,
-        kWPutere: kWPutere ? kWPutere : caiPutere / 1.36,
+        caiPutere: caiPutere ? caiPutere : (kWPutere * 1.36).toFixed(2),
+        kWPutere: kWPutere ? kWPutere : (kWPutere * 1.36).toFixed(2),
         clientId: id
     });
     car.save()
@@ -53,8 +52,8 @@ exports.postAddCar = (req, res, next) => {
             anulFabricatiei,
             tipMotorizare,
             capacitateMotor,
-            caiPutere: caiPutere ? caiPutere : kWPutere * 1.36,
-            kWPutere: kWPutere ? kWPutere : caiPutere / 1.36,
+            caiPutere: caiPutere ? caiPutere : (kWPutere * 1.36).toFixed(2),
+            kWPutere: kWPutere ? kWPutere : (kWPutere * 1.36).toFixed(2),
         }
     });
 }
